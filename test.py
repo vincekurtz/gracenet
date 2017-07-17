6643d9eb92758214c945bedda2e393c93b966489
Vince K <vjkurtz@gmail.com>
2017-07-17 10:33:17 -0400

    new nn features. PCA, artificial data, learning curves, etc

--- test.py
+++ test.py
@@ -18,6 +18,7 @@ from train import load_data, fit_sines
 import matplotlib.pyplot as plt
 from sin_approx import predict_next, get_sinusoid_params
 from sklearn.metrics import r2_score
+import numpy as np
 
 def plot_stuff(x_example, y_example, predicted):
     """
@@ -51,22 +52,27 @@ if __name__=="__main__":
     # load testing data
     X, y = load_data("training_data.json")
 
-    print("===> Plotting data")
-    N = 8000  # numper of samples to deal with for now
-    precip = [X[i][0] for i in range(N)]
-    temp = [X[i][1] for i in range(N)]
-    veg = [X[i][2] for i in range(N)]
-    lat = [X[i][3] for i in range(N)]
-    anomoly = [y[i] for i in range(N)]
+    #print("===> Plotting data")
+    #N = 8000  # numper of samples to deal with for now
+    #precip = [X[i][0] for i in range(N)]
+    #temp = [X[i][1] for i in range(N)]
+    #veg = [X[i][2] for i in range(N)]
+    #lat = [X[i][3] for i in range(N)]
+    #anomoly = [y[i] for i in range(N)]
 
-    X_vars = [X[i] for i in range(N)]
-    #scatterplot(lat, anomoly)
+    #X_vars = [X[i] for i in range(N)]
+    #scatterplot(veg, anomoly)
 
     # load the network
+    print("===> Loading Network")
     rgr = joblib.load('parameters.pkl')
     # predict on the network
-    pred = rgr.predict(X_vars)
+    print("===> Predicting on Network")
+    pred = rgr.predict(X)
+    print(pred.shape)
+    print(np.asarray(y).shape)
+    print(np.asarray(X).shape)
     # get r^2 score
-    r2 = r2_score(anomoly, pred)
+    r2 = r2_score(y, pred)
     
     print(r2)
--- train.py
+++ train.py
@@ -14,8 +14,13 @@
 # See http://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPRegressor.html
 from sklearn.neural_network import MLPRegressor
 from sklearn.externals import joblib  # for saving parameters
+from sklearn.preprocessing import StandardScaler  # for feature scaling
 import json
 from sin_approx import get_sinusoid_params
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA  # principle component analysis for dim. reduction
+import timeit   # timer
 
 train_data = "/home/vince/Groundwater/NeuralNet/training_data.json"
 
@@ -25,27 +30,27 @@ def load_data(datafile):
     Return X, y, where X is the 12d previous months input
     and y is the (1d) anomoly.
     """
-    print("===> Loading Data")
     with open(datafile, 'r') as f:
         data = json.load(f)
         X = data['X']
         y = data['y']
-    return (X, y)
 
-def create_and_train(X, y):
+    X = np.asarray(X)
+    y = np.asarray(y)
+
+    return (X, y)   # load as np arrays
+
+def create_and_train(X, y, alpha=1e-5):
     """
-    Create the network with certain parameters.
-    then train it with the given data.
+    Create the network with certain parameters.  then train it with the given data.
     """
-    print("===> Creating Network")
     net = MLPRegressor(
-            solver='sgd',
-            alpha=1e-5,
-            activation='logistic',
-            hidden_layer_sizes=(50,20),
+            solver='adam',
+            alpha=alpha,
+            activation='relu',
+            hidden_layer_sizes=(500,500),
             random_state=1)
 
-    print("===> Training Network")
     net.fit(X, y)
 
     return net
@@ -64,25 +69,105 @@ def fit_sines(prev_anomolies):
         fits.append(fit)
     return fits
 
-if __name__=="__main__":
+def artificial_data(X, y):
+    """
+    Generate a new row of data with similar
+    characteristics, but gaussian noise added so
+    that we can get more data points.
+    """
+    # Generate arrays of random fractions  (~ .9 to 1.1)
+    x_rand_perc = 0.1 * np.random.standard_normal(X.shape) + 1  # gaussian with mean 1, std 0.1
+    y_rand_perc = 0.1 * np.random.standard_normal(y.shape) + 1
+
+    # Multiply random fractions by original to get new dataset
+    X_new = np.multiply(X, x_rand_perc)
+    y_new = np.multiply(y, y_rand_perc)
+
+    return (X_new, y_new)
+
+def choose_alpha():
+    """
+    Make plot of testing and training r^2 values for 
+    different values of the regularization parameter.
+    """
+    test = []
+    train = []
+
+    alphas = [3e-3, 1e-2, 3e-2, 1e-1, 3e-1, 1, 3, 10]
+    for alpha in alphas:
+        print("")
+        print(" ALPHA = %s" % alpha)
+        print("")
+        (R_train, R_test) = main(save_data=False, alpha=alpha)
+        train.append(R_train)
+        test.append(R_test)
+    plt.semilogx(alphas, train, 'ro', label='training set')
+    plt.semilogx(alphas, test, 'bx', label='test set')
+    plt.xlabel('alpha')
+    plt.ylabel('R^2 score')
+    plt.legend()
+    plt.show()
+
+def main(save_data=True, alpha=1e-5):
+    """
+    Train the network and print basic R^2 scores
+    for training and test sets. Then save the parameters
+    """
+
+    print("===> Loading Data")
     X, y = load_data("training_data.json")
-    N = 1000000  # number of samples to train on
-    print("Total samples availible = %s" % len(X))
-    print("Samples used = %s" % N)
+    X_test, y_test = load_data("testing_data.json")   # for cross validation
 
-    # just take a subset of availible data for now
-    X_in = [X[i] for i in range(N)]
-    anomoly = [y[i] for i in range(N)]
+    # perform feature scaling
+    print("===> Scaling features")
+    scaler = StandardScaler()
+    scaler.fit(X)  # Don't cheat - fit only on training data
+    X = scaler.transform(X)
+    X_test = scaler.transform(X_test)   # apply same transformation to test data
 
-    # calculate sinusoid parameters based on the previous anomolies. 
-    # we'll use the slope from these to train the network
-    #sine_params = fit_sines(prev_anomolies)
-    #slopes = [i[3] for i in sine_params]  # the slope is the third parameter
+    print("  Total samples availible = %s" % len(y))
 
-    rgr = create_and_train(X_in, anomoly)
+    # generate artificial data
+    target_samples = 100   # how many training examples we'd like to have
+    original_X = X
+    original_y = y
+    while (len(y) < target_samples):
+        newx, newy = artificial_data(original_X, original_y)  # only base generated data on the originals
+        X = np.concatenate((X, newx))
+        y = np.concatenate((y, newy))
+        print("    generating artificial data ... %s / %s samples ready" % (len(y), target_samples))
 
-    # save the weights/parameters
-    joblib.dump(rgr, 'parameters.pkl')
+    print("  Total samples with artificial = %s" % len(y))
 
+    # dimensionality reduction/principle component analysis
+    print("===> Reducing Dimensions")
+    print("  old training dimensions (n_samples, n_features): (%s, %s) " % (X.shape[0], X.shape[1]))
+    pca = PCA()
+    pca.fit(X)
+    X = pca.transform(X)  # apply the dim. reduction to both training and test sets
+    X_test = pca.transform(X_test)
+    print("  new training dimensions (n_samples, n_features): (%s, %s) " % (X.shape[0], X.shape[1]))
 
+    print("===> Creating and Training Network")
+    start_time = timeit.default_timer()   # time the training process
+    rgr = create_and_train(X, y, alpha)
+    end_time = timeit.default_timer()
+    print("  %s seconds to train" % (end_time - start_time))
+    print("  %s iterations completed" % (rgr.n_iter_))
 
+    print("===> Evaluating Performance")
+    training_R = rgr.score(X, y)
+    testing_R = rgr.score(X_test, y_test)
+    print("")
+    print("Training data R^2 score: %s" % training_R)
+    print("Testing data R^2 score: %s" % testing_R)
+    print("")
+
+    if save_data:  # so you can switch this off sometimes
+        print("===> Saving parameters")
+        joblib.dump(rgr, 'parameters.pkl')
+
+    return (training_R, testing_R)
+
+if __name__=="__main__":
+    choose_alpha()
